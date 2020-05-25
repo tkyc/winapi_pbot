487567ea7898c68de97b980fd456e3b0c8ddf4cd
unknown <terrykyc.7000@gmail.com>
2020-05-24 18:56:21 -0700

    Experimented with sending keys; experimenting with getting window title

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::mem;
+use std::thread::sleep;
+use std::time::Duration;
+use winapi;
 use winapi::ctypes::{ wchar_t };
-use winapi::um::winnt::{ HANDLE, LPCWSTR, WCHAR };
-use winapi::um::winuser::{ WNDENUMPROC, EnumWindows, FindWindowW, GetWindowThreadProcessId };
+use winapi::um::winnt::{ HANDLE, LPCWSTR, WCHAR, CHAR };
+use winapi::um::winuser::{ WNDENUMPROC, EnumWindows, FindWindowW, GetWindowThreadProcessId,
+                           PostThreadMessageW, PostMessageW, SendMessageW, SetForegroundWindow, WM_KEYDOWN, VK_LEFT, WM_KEYUP, INPUT, INPUT_u, INPUT_KEYBOARD,
+                           KEYBDINPUT, PostMessageA, PostThreadMessageA, SendMessageA, GUITHREADINFO, GetGUIThreadInfo, GetWindowTextA };
 use winapi::shared::minwindef::{ MAX_PATH, DWORD, LPARAM, BOOL, TRUE, FALSE };
 use winapi::shared::windef::{ HWND };
 use winapi::um::handleapi::INVALID_HANDLE_VALUE;
@@ -13,8 +18,9 @@ use winapi::um::memoryapi::ReadProcessMemory;
 
 const PROCESS_WM_READ: u32 = 0x010;
 
-pub struct DesignatedHwnd {
+pub struct TargetWindow {
     dw_proc_id: DWORD, //u32
+    dw_thread_id: DWORD, //u32
     hwnd: HWND, //*mut HWND__
 }
 
@@ -25,38 +31,85 @@ unsafe extern "system" fn get_window_by_proc(id: DWORD) -> HWND {
     )
 }
 
-//BOOL -> c_int -> i32 
 unsafe extern "system" fn enum_wnd_proc(hwnd: HWND, l_param: LPARAM) -> BOOL {
 
     //Process id of current hwnd
     let mut dw_proc_id: DWORD = 0x0;
 
-    let designated_hwnd_ptr = l_param as *mut DesignatedHwnd;
+    let target_wnd_ptr = l_param as *mut TargetWindow;
 
     //let dw_proc_id_ptr: *mut DWORD = &mut dw_proc_id; //--- Raw pointer
 
-    //Gets the current process id of hwnd
+    //Gets the current process id of hwnd --- LPDWORD -> DWORD
     //GetWindowThreadProcessId(hwnd, dw_proc_id_ptr);
-    GetWindowThreadProcessId(hwnd, &mut dw_proc_id);
 
-    if (*designated_hwnd_ptr).dw_proc_id == dw_proc_id {
+    //Gets the current process id of hwnd ---- LPDWORD -> DWORD
+    (*target_wnd_ptr).dw_thread_id = GetWindowThreadProcessId(hwnd, &mut dw_proc_id);
 
-        (*designated_hwnd_ptr).hwnd = hwnd;
+    if (*target_wnd_ptr).dw_proc_id == dw_proc_id {
 
+        (*target_wnd_ptr).hwnd = hwnd;
+
+        println!("HWND found: {:?}", dw_proc_id);
+
+        //Found hwnd --- FALSE -> i32
         return FALSE
+
     }
 
-    //Continue
+    println!("HWND not found: {:?}", dw_proc_id);
+
+    //Continue enumeration w/ EnumWindows --- TRUE -> i32
     TRUE
+
+}
+
+pub unsafe extern "system" fn send_key_to(window: &TargetWindow) {
+
+    //Get the window belonging to the hwnd
+    //let mut window_name_buf: [i8; 128] = [0x0; 128];
+    //let mut ptr: *mut CHAR = &mut window_name_buf;
+    let mut window_name_buffer = Vec::with_capacity(1024);
+
+    let f = GetWindowTextA(window.hwnd, window_name_buffer.as_mut_ptr(), 1024);
+
+    println!("Found window with the following name: {:?}", f);
+
+    //Set focus to window then delay key press
+    SetForegroundWindow(window.hwnd);
+
+    sleep(Duration::from_millis(500));
+
+    //Get thread that owns hwnd
+    let thread_info: *mut GUITHREADINFO = std::ptr::null_mut(); //--- Raw pointer
+
+    match GetGUIThreadInfo(window.dw_thread_id, thread_info) {
+
+        0 => println!("Thread info not found..."),
+
+        1 => println!("Thread info found -- focusing on hwnd: {:?}", (*thread_info).hwndFocus),
+
+        _ => (),
+
+    };
+
+    PostThreadMessageA(window.dw_thread_id, WM_KEYDOWN, 0x31, 0x2);
+    PostMessageA(window.hwnd, WM_KEYDOWN, 0x31, 0x2);
+    PostMessageA(window.hwnd, WM_KEYUP, 0x31, 0x2);
+    SendMessageA(window.hwnd, WM_KEYDOWN, 0x31, 0x2);
+    println!("Sending key for PID: {:?} --- TID: {:?}", window.dw_proc_id, window.dw_thread_id);
 
 }
 
 //TODO: Free memory
 fn main() {
 
-    let mut designated_hwnd: DesignatedHwnd = unsafe {
-        DesignatedHwnd {
-            dw_proc_id: 0x0 as u32,
+    const TARGET_DW_PROC_ID: DWORD = 0xD3C;
+
+    let mut target: TargetWindow = unsafe {
+        TargetWindow {
+            dw_proc_id: 10132,
+            dw_thread_id: 0x0,
             hwnd: FindWindowW(
                 0x0 as *const WCHAR as LPCWSTR,
                 0x0 as *const WCHAR as LPCWSTR,
@@ -64,7 +117,7 @@ fn main() {
         }
     };
 
-    //let designated_hwnd_ptr: *mut DesignatedHwnd = &mut designated_hwnd; //--- Raw pointer
+    //let target_hwnd: *mut TargetWindow = &mut designated_hwnd; //--- Raw pointer
 
     let hwnd: HWND = unsafe {
         FindWindowW(
@@ -73,55 +126,62 @@ fn main() {
         )
     };
 
-    unsafe { enum_wnd_proc(hwnd, &mut designated_hwnd as *mut DesignatedHwnd as LPARAM) };
+    unsafe { EnumWindows(Some(enum_wnd_proc), &mut target as *mut TargetWindow as LPARAM) };
 
-    let snapshots: HANDLE = unsafe { CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0) };
+    unsafe { send_key_to(&target) };
 
-    if snapshots == INVALID_HANDLE_VALUE {
-        
-        println!("Invalid handle value...");
-        
-        println!("Handle value: {:?}", snapshots);
-        
-        return
-        
-    }
+    //loop {
 
-    let pe32: *mut PROCESSENTRY32 = &mut PROCESSENTRY32 {
-        dwSize: mem::size_of::<PROCESSENTRY32>() as u32,
-        cntUsage: 0,
-        th32ProcessID: 0,
-        th32DefaultHeapID: 0,
-        th32ModuleID: 0,
-        cntThreads: 0,
-        th32ParentProcessID: 0,
-        pcPriClassBase: 0,
-        dwFlags: 0,
-        szExeFile: [0; MAX_PATH],
-    };
+    //    unsafe { send_key_to(&target) };
 
-    //let pe32_ptr: *mut PROCESSENTRY32 = &mut pe32; //--- Raw pointer
+    //}
+    //let snapshots: HANDLE = unsafe { CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0) };
+
+    //if snapshots == INVALID_HANDLE_VALUE {
+    //    
+    //    println!("Invalid handle value...");
+    //    
+    //    println!("Handle value: {:?}", snapshots);
+    //    
+    //    return
+    //    
+    //}
+
+    //let pe32: *mut PROCESSENTRY32 = &mut PROCESSENTRY32 {
+    //    dwSize: mem::size_of::<PROCESSENTRY32>() as u32,
+    //    cntUsage: 0,
+    //    th32ProcessID: 0,
+    //    th32DefaultHeapID: 0,
+    //    th32ModuleID: 0,
+    //    cntThreads: 0,
+    //    th32ParentProcessID: 0,
+    //    pcPriClassBase: 0,
+    //    dwFlags: 0,
+    //    szExeFile: [0; MAX_PATH],
+    //};
+
+    ////let pe32_ptr: *mut PROCESSENTRY32 = &mut pe32; //--- Raw pointer
     
-    while unsafe { Process32Next(snapshots, pe32) == 1 } {
+    //while unsafe { Process32Next(snapshots, pe32) == 1 } {
 
-        let mut pe32_name: String = String::new();
-        
-        //TODO: Data parallelization (chunks_exact)
-        for c in unsafe { (*pe32).szExeFile.iter() } {
+    //    let mut pe32_name: String = String::new();
+    //    
+    //    //TODO: Data parallelization (chunks_exact)
+    //    for c in unsafe { (*pe32).szExeFile.iter() } {
 
-            match *c {
+    //        match *c {
 
-                0 => break,
+    //            0 => break,
 
-                _ => pe32_name.push(*c as u8 as char),
+    //            _ => pe32_name.push(*c as u8 as char),
 
-            }
-                
-        }
+    //        }
+    //            
+    //    }
 
-        println!("{:?} --- {:?}", pe32_name, unsafe { (*pe32).th32ProcessID });
+    //    println!("{:?} --- {:?}", pe32_name, unsafe { (*pe32).th32ProcessID });
 
-    }
+    //}
 
 
    //TODO: https://codingvision.net/security/c-read-write-another-process-memory 
@@ -129,4 +189,8 @@ fn main() {
    //TODO: https://users.rust-lang.org/t/comparing-a-string-to-an-array-of-i8/5120/4
    //let process_handle: HANDLE = OpenProcess(PROCESS_WM_READ, 0, dwProcessId: DWORD);
    //https://stackoverflow.com/questions/12099957/how-to-send-a-keystroke-to-an-other-process-ex-a-notepad
+   //https://stackoverflow.com/questions/20162359/c-best-way-to-get-window-handle-of-the-only-window-from-a-process-by-process
+   //https://github.com/retep998/winapi-rs/issues/746
+   //https://dailyoverdose.wordpress.com/2009/10/28/postmessage-and-sendmessage/
+   //https://stackoverflow.com/questions/11890972/simulating-key-press-with-postmessage-only-works-in-some-applications
 }
